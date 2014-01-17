84c736258ba458bc7ac2146d90b00b7395a18ab7
Dexter <tyrannosaurusdex@gmail.com>
2014-01-17 15:15:18 -0600

    week 1 Dev/Iowa Bootcamp

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".img-thumbnail").parent().on("mouseenter", function(){
+    console.log(this);
+    $(this).find(".describe").fadeIn();
+});
+
+$(".img-thumbnail").parent().on("mouseleave", function(){
+    $(this).find(".describe").fadeOut();
+}); 
